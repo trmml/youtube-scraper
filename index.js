965ac80996da48750d47e015022904929fa089da
Josh Trommel <joshtrommel@gmail.com>
2016-11-20 20:43:44 -0800

    A super tiny cute API is done

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express')
+	, request = require('request')
+	, cheerio = require('cheerio');
+
+var app = express();
+
+function buildURL(query) {
+	return "https://www.youtube.com/results?search_query=" + encodeURI(query)
+}
+
+app.get('/', function(req, res) {
+	res.send({hello: 'world'});
+});
+
+app.get('/search', function(req, res) {
+	var query = req.query.q
+		, url = buildURL(query);
+
+	request.get(url, function(err, response, body) {
+		if (err || !response.statusCode == 200) return res.send({error: err});
+
+		var $ = cheerio.load(body);
+		var videoURL = $('h3.yt-lockup-title a').attr('href');
+
+		videoURL = "https://youtube.com" + videoURL;
+		res.send(videoURL)
+	});
+});
+
+app.listen(3000, function() {
+	console.log('running on localhost:3000');
+});
